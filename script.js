3bf6a7e1b6a5f8a27a8f0bb52140252d952ae629
kevlin94 <30875242+kevlin94@users.noreply.github.com>
2017-08-14 08:56:40 -1000

    java script userName

---
+++ script.js
@@ -0,0 +1,11 @@
+function sayHello() {
+  //I create a function called sayhello//
+  var userName = prompt("what is your name?");
+  //I create a variable called userName and ask the user something//
+  window.alert("Hello " + userName);
+  //I create a popup window telling the userName Hello//
+  
+  
+  
+  
+}
