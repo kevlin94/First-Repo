f2da7641aa5425723a33777b963feacb0ec5cab0
kevlin94 <30875242+kevlin94@users.noreply.github.com>
2017-08-16 09:10:40 -1000

    Add files via upload

---
+++ numberindex.js
@@ -0,0 +1,7 @@
+var favFoods = ["bacon","Lettuce","Tomato"];
+var newFood = prompt("what do you want to replace bacon with");
+//ask user and save as newFood
+favFoods[0]=newFood;
+//replace favfood0 with new food
+window.alert(favFoods)
+//output all values
