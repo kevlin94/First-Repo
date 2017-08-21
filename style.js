833fee4ed4b013a15b8177293fedabcb93279888
kevlin94 <30875242+kevlin94@users.noreply.github.com>
2017-08-21 09:03:43 -1000

    Random insult generator

---
+++ style.js
@@ -0,0 +1,15 @@
+function sayHello(){
+  
+  var insultGen=[
+  "u suck",
+  "do better",
+  "get good grade",
+  "goto school",
+  "try harder",
+  "insult 6",
+  "insult 7",
+  "insult 8",
+  "insult 9"
+  ]
+
+window.alert(insultGen[Math.floor(Math.random()*9)]);}
