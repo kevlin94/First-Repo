ad4dd1972f7a820ac9c8d430a3f37e1962b7139e
kevlin94 <30875242+kevlin94@users.noreply.github.com>
2017-08-23 09:20:33 -1000

    Add files via upload

---
+++ calculatingarea.js
@@ -0,0 +1,14 @@
+function calculateArea(width,height){
+var area=width*height;
+//multiplies width and height
+
+return area;
+
+}
+var x=prompt("what is the width");
+var y=prompt("what is the length");
+var wallOne=calculateArea(x,y);
+
+
+
+window.alert(wallOne);
