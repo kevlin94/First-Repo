a91d97606b7a15d0cb6cf8c552b201b8e18e0927
kevlin94 <30875242+kevlin94@users.noreply.github.com>
2017-12-18 08:03:35 -1000

    Add files via upload

--- restaurant.js
+++ restaurant.js
@@ -79,6 +79,38 @@ function buycoffee(){
 }
 
 function receipt() {
+  var quantity1=document.getElementById('quantity1').value;
+  var price1=quantity1*food1.cost;
+  localStorage.setItem('honeydew',food1.name);
+  localStorage.setItem('honeydewprice',price1);
+  
+  var quantity2=document.getElementById('quantity2').value;
+  var price2=quantity2*food2.cost;
+  localStorage.setItem('mango',food2.name);
+  localStorage.setItem('mangoprice',price2);
+  
+  var quantity3=document.getElementById('quantity3').value;
+  var price3=quantity3*food1.cost;
+  localStorage.setItem('milktea',food3.name);
+  localStorage.setItem('milkteaprice',price3);
+  
+  var quantity4=document.getElementById('quantity4').value;
+  var price4=quantity4*food4.cost;
+  localStorage.setItem('lychee',food4.name);
+  localStorage.setItem('lycheeprice',price4);
+  
+  var quantity5=document.getElementById('quantity5').value;
+  var price5=quantity5*food5.cost;
+  localStorage.setItem('strawberrybanana',food5.name);
+  localStorage.setItem('strawberrybananaprice',price5);
+  
+  var quantity6=document.getElementById('quantity6').value;
+  var price6=quantity6*food6.cost;
+  localStorage.setItem('coffee',food6.name);
+  localStorage.setItem('coffeeprice',price6);
+  
+  
+  
    location.href="restaurant2.html";
 }
 
@@ -96,11 +128,14 @@ function getTotal() {
   var k=localStorage.getItem("coffee");
   var l=localStorage.getItem("coffeeprice");
   var m=parseInt(b)+parseInt(d)+parseInt(f)+parseInt(h)+parseInt(j)+parseInt(l);
+  var n=1.04712*m;
+  var o=n.toFixed(2);
+
 
   
   
   
-  document.getElementById('total').innerHTML=a+" $"+b+"<br>" +c+" $"+d+"<br>" +e+" $"+f+"<br>"+g +" $"+h+ "<br>" +i+" $"+j+"<br>" +k+" $"+l + "<br>"+"Total $" +m;
+  document.getElementById('total').innerHTML=a+" $"+b+"<br>" +c+" $"+d+"<br>" +e+" $"+f+"<br>"+g +" $"+h+ "<br>" +i+" $"+j+"<br>" +k+" $"+l + "<br>"+"Total $" +o;
 
   
   
